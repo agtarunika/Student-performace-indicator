a020f73f1083059a4651db2eaafc32c5be72c3af
Tarunika Agarwal <tarunika1607@gmail.com>
2025-07-05 14:15:13 +0530

    steup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path):
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='tarunika',
+    author_email='tarunika1607@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
